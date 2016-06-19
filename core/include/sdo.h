7b59e90848d82235fc114799589185a23a12c96a
Adrian Weiler <a.weiler@aldea.de>
2016-06-19 14:40:22 +0200

    Add missing header for std::array<>

--- core/include/sdo.h
+++ core/include/sdo.h
@@ -28,9 +28,10 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
- 
+
 #pragma once
 
+#include <array>
 #include <functional>
 #include <vector>
 #include <mutex>
